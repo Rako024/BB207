e5b35e4007c0e156d9fdcdf4c038abdb7b4e13e3
Rashid Babazada <116404895+Rako024@users.noreply.github.com>
2024-03-12 00:26:00 +0400

    12.03.2024 Task 3 completed

---
+++ Task3/task3 app/task3 app/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace task3_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region task1
+            //int[]arr = { 4, 5, 6, 7, 1, 2, 3, 12 };
+            //int min = arr[0];
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (min > arr[i])
+            //    {
+            //        min = arr[i];
+            //    }
+            //}
+            //Console.WriteLine(min);
+            #endregion
+
+
+            #region task2
+            //string[] strings = { "Yusif", "Resul", "Enver" };
+            //Console.Write("index daxil et = ");
+            //int index = int.Parse(Console.ReadLine());
+            //if (index < 0 || index >= strings.Length)
+            //{
+            //    Console.WriteLine("duzgun index daxil edin!!!");
+            //}
+            //else
+            //{
+            //    string word = strings[index];
+            //    for (int i = word.Length - 1; i >= 0; i--)
+            //    {
+            //        Console.Write(word[i]);
+            //    }
+            //}
+            #endregion
+
+            #region task3
+
+            //int[] a = { -2, 3, 4, -5 };
+            //int[] b = new int[a.Length];
+            //for(int i = a.Length-1; i >= 0; i--)
+            //{
+            //    if (a[i] < 0)
+            //    {
+            //        b[a.Length-1-i] = -a[i];
+            //        Console.Write(-a[i]+" ");
+            //    }
+            //    else
+            //    {
+            //        b[a.Length - 1 - i] = a[i];
+            //        Console.Write(a[i]+" ");
+            //    }
+            //}
+
+            #endregion
+
+            #region task4
+            //int[] arr = { -2, 3, 4, -5 };
+            //string word = "";
+            //for(int i = 0; i < arr.Length; i++)
+            //{
+            //    word = word + arr[i]+" ";
+            //}
+            //Console.WriteLine(word);
+            #endregion
+        }
+    }
+}
