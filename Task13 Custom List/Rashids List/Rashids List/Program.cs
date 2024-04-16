6802a828d90878d27210c1556bdf96c9ab997de9
Rashid Babazada <116404895+Rako024@users.noreply.github.com>
2024-04-17 03:45:29 +0400

    17.04.2024 Custom list Completed Task

---
+++ Task13 Custom List/Rashids List/Rashids List/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Rashids_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>();
+            Console.WriteLine(list.Count+" "+list.Capacity);
+            list.Add(0);
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            Console.WriteLine(list.Count+" "+list.Capacity);
+            RashidsList<int> ints = new RashidsList<int>();
+            ints.Add(0);
+            ints.Add(1);
+            ints.Add(2);
+            ints.Add(3);
+            Console.WriteLine($"Rashid's List Cout = {ints.Count} Capacity = {ints.Capacity}");
+            
+            ints.Inset(12, 2);
+            ints.Inset(12, 4);
+            ints.CustomReverse(2,4);
+            Console.WriteLine($"Rashid's List Cout = {ints.Count} Capacity = {ints.Capacity}");
+            foreach (int i in ints)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("index of = "+ints.IndexOf(12)+" lastindexof = "+ints.LastIndexOf(12));
+        }
+    }
+}
---
+++ Task13 Custom List/Rashids List/Rashids List/RashidsList.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rashids_List
+{
+    public class RashidsList<T> : IEnumerable<T>
+    {
+        private T[] _items;
+        private int _count;
+        public int Count {  get { return _count; } }
+
+        private int _capacity;
+
+        public int Capacity
+        {
+            get { return _capacity; }
+        }
+
+        //inexer
+        public T this[int index] { 
+            get 
+            {
+                if(index < 0 || index >= _count)
+                {
+                    throw new IndexOutOfRangeException("Duz emmeli index gir! araliqda bele index yoxdu!");
+                }
+                return _items[index];
+            }
+            set
+            {
+                if (index < 0 || index >= _count)
+                {
+                    throw new IndexOutOfRangeException("Duz emmeli index gir! araliqda bele index yoxdu!");
+                }
+                _items[index] = value;
+            }
+        }
+
+        public RashidsList()
+        {
+            _items = new T[0];
+            _count = 0;
+            _capacity = 0;
+        }
+
+        public void Add(T item)
+        {
+            if(_items.Length == 0)
+            {
+                _capacity = 4;
+                Array.Resize(ref _items, _capacity);
+            }
+            if(_count == _items.Length)
+            {
+                _capacity = _items.Length * 2;
+                Array.Resize(ref _items, _capacity);
+
+            }
+            _items[_count] = item;
+            _count++;
+        }
+        public int IndexOf(T item)
+        {
+            return Array.IndexOf(_items, item,0,_count);
+        }
+        public int LastIndexOf(T item)
+        {
+            return Array.LastIndexOf(_items, item);
+        }
+
+        public bool Contains(T item)
+        {
+            return _count != 0 && IndexOf(item) != -1;
+        }
+        public bool Remove(T item)
+        {
+            int index = IndexOf(item);
+            if (index >= 0)
+            {
+                RemoveAt(index);
+                return true;
+            }
+            return false;
+        }
+
+        public void RemoveAt(int index)
+        {
+            if (index < 0 || index >= _count)
+            {
+                throw new IndexOutOfRangeException("Bu Index istenilen araliqda deyil!");
+            }
+
+            for (int i = index; i < _count - 1; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+
+            _count--;
+        }
+
+        public void CustomReverse(int index, int count)
+        {
+            if (index < 0 || index >= _count)
+            {
+                throw new IndexOutOfRangeException("Bu Index istenilen araliqda deyil!");
+            }
+            Array.Reverse(_items, index, count);
+        }
+        public void CustomReverse()
+        {
+
+            Array.Reverse(_items, 0, _count);
+        }
+
+        public T[] ToArray()
+        {
+            T[] arr = new T[_count];
+             Array.Copy(_items, arr, _count);
+            return arr;
+        }
+
+        public void Clear()
+        {
+            int count = _count;
+            _count = 0;
+            Array.Clear(_items, 0, count);
+        }
+
+        public void Inset(T item, int index)
+        {
+            if (index < 0 || index > _count)
+            {
+                throw new IndexOutOfRangeException("Duz emmeli index gir! araliqda bele index yoxdu!");
+            }
+            if (index == _count)
+            {
+                Add(item);
+            }
+            else
+            {
+                if (_items.Length == 0)
+                {
+                    _capacity = 4;
+                    Array.Resize(ref _items, _capacity);
+                }
+
+                if (_count == _capacity)
+                {
+                    _capacity = _items.Length * 2;
+                    Array.Resize(ref _items, _capacity);
+                }
+                for(int i = _count; i > index; i--)
+                {
+                    _items[i] = _items[i-1];
+                }
+                _items[index] = item;
+                _count++;
+
+            }
+
+        }
+
+
+        //forEach ucun lazimli methodlar
+        public IEnumerator<T> GetEnumerator()   
+        {
+            for(int i = 0; i< _count; i++)
+            {
+                yield return _items[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
